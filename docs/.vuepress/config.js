087dc124da8448ea1ca17df4f820f3a9cc174e08
雷明庆 <mingqing.lei@qq.com>
2019-09-17 13:39:31 +0800

    尝试侧边栏和导航栏

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,5 +1,29 @@
 module.exports = {
   title: '雷明庆的博客',
   description: '保持简单',
-  base: '/blog/'
+  base: '/blog/',
+
+  themeConfig: {
+    nav: [
+      { text: 'Home', link: '/' },
+      { text: 'Guide', link: '/guide/' },
+      { text: 'External', link: 'https://google.com' },
+      {
+        text: 'Languages',
+        ariaLabel: 'Language Menu',
+        items: [
+          { text: 'Chinese', link: '/language/chinese/' },
+          { text: 'Japanese', link: '/language/japanese/' }
+        ]
+      }
+    ]
+  },
+
+  themeConfig: {
+    sidebar: [
+      '/',
+      '/page-a',
+      ['/page-b', 'Explicit link text']
+    ]
+  }
 }
